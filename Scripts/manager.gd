3ae08f86f005e32cfb34482d644d22b95e7779a5
Ewan Durand <ewan-dur-23@hotmail.fr>
2025-12-15 11:44:22 +0100

    Scripts d'expérience générés

--- Scripts/manager.gd
+++ Scripts/manager.gd
@@ -8,12 +8,20 @@ var exp_order : Array = []
 var order_int : int
 var start_first : bool = true
 var start_second : bool = false
-var color_exp_script : Array  = [
-	[0, 3],
-	[2, 1]]
-var word_exp_script : Array  = [
-	[1, 2],
-	[2, 0]]
+var color_exp_script : Array = [
+	[0, 1], [3, 1], [3, 2], [3, 2], [1, 3], 
+	[2, 1], [2, 1], [1, 2], [3, 1], [0, 3], 
+	[3, 0], [2, 0], [0, 1], [3, 1], [1, 0], 
+	[3, 1], [0, 2], [2, 3], [0, 2], [2, 1], 
+	[1, 0], [1, 2], [1, 2], [0, 1], [3, 2]
+	]
+var word_exp_script : Array = [
+	[1, 0], [0, 3], [3, 1], [0, 1], [1, 0], 
+	[1, 3], [1, 3], [2, 3], [2, 1], [3, 2], 
+	[2, 0], [3, 0], [1, 0], [1, 2], [2, 3], 
+	[1, 2], [3, 2], [2, 3], [3, 2], [2, 1], 
+	[2, 3], [0, 2], [1, 3], [2, 0], [3, 2]
+	]
 var scn
 
 func init(exp : int) -> void:
